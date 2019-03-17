7f36ab4c1238bdab89e0fb20c169d59b5b136ca7
Tiago Sampaio <tiago@tiagosampaio.com>
2019-03-16 21:26:22 -0300

    Added improvements.

--- app/code/community/Frenet/Shipping/Model/Config/Source/Product/Attributes.php
+++ app/code/community/Frenet/Shipping/Model/Config/Source/Product/Attributes.php
@@ -39,6 +39,10 @@ class Frenet_Shipping_Model_Config_Source_Product_Attributes
         if (empty($this->options)) {
             /** @var Mage_Catalog_Model_Resource_Eav_Attribute $attribute */
             foreach ($this->getProductAttributes() as $attribute) {
+                if (!$this->validateAttribute($attribute)) {
+                    continue;
+                }
+
                 $this->options[$attribute->getAttributeCode()] = $attribute->getFrontendLabel();
             }
         }
@@ -47,13 +51,29 @@ class Frenet_Shipping_Model_Config_Source_Product_Attributes
     }
 
     /**
-     * @return array
+     * @param Mage_Catalog_Model_Resource_Eav_Attribute $attribute
+     * @return bool
+     */
+    private function validateAttribute(Mage_Catalog_Model_Resource_Eav_Attribute $attribute)
+    {
+        if (!$attribute->getAttributeCode()) {
+            return false;
+        }
+
+        if (!$attribute->getFrontendLabel()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return Mage_Catalog_Model_Resource_Product_Attribute_Collection
      */
     private function getProductAttributes()
     {
-        /** @var Mage_Catalog_Model_Product $product */
-        $attributes = Mage::getSingleton('catalog/config')->getProductAttributes();
-
-        return $attributes;
+        /** @var Mage_Catalog_Model_Resource_Product_Attribute_Collection $collection */
+        $collection = Mage::getResourceModel('catalog/product_attribute_collection');
+        return $collection;
     }
 }
---
+++ app/code/community/Frenet/Shipping/Model/Resource/Setup.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class Frenet_Shipping_Model_Resource_Setup
+ */
+class Frenet_Shipping_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
+{
+
+}
