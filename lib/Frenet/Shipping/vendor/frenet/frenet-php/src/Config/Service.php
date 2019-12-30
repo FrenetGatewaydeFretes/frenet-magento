7d749a7feefbedfdcbd1898b05ec5bfabc1f7f81
Tiago Sampaio <tiago@tiagosampaio.com>
2019-12-30 16:59:16 -0300

    Dependency update.

--- lib/Frenet/Shipping/vendor/frenet/frenet-php/src/Config/Service.php
+++ lib/Frenet/Shipping/vendor/frenet/frenet-php/src/Config/Service.php
@@ -2,6 +2,8 @@
 
 namespace Frenet\Config;
 
+use Frenet\Framework\Exception\WrongDataTypeException;
+
 /**
  * Class Service
  *
@@ -10,6 +12,13 @@ namespace Frenet\Config;
 class Service implements ConfigInterface
 {
     /**
+     * @var array
+     */
+    private $validProtocols = [
+        'http', 'https'
+    ];
+
+    /**
      * @var string
      */
     private $protocol = 'http';
@@ -17,7 +26,12 @@ class Service implements ConfigInterface
     /**
      * @var string
      */
-    private $host = 'api.frenet.com.br';
+    private $defaultHost = 'api.frenet.com.br';
+
+    /**
+     * @var string
+     */
+    private $host = null;
 
     /**
      * @return string
@@ -28,10 +42,109 @@ class Service implements ConfigInterface
     }
 
     /**
+     * @param string $protocol
+     *
+     * @return $this
+     *
+     * @throws WrongDataTypeException
+     */
+    public function setProtocol($protocol)
+    {
+        $protocol = trim(strtolower($protocol));
+
+        if (!$this->validateProtocol($protocol)) {
+            throw new WrongDataTypeException('Invalid protocol provided.');
+        }
+
+        $this->protocol = $protocol;
+
+        return $this;
+    }
+
+    /**
      * @return string
      */
     public function getHost()
     {
-        return (string) $this->host;
+        return $this->getHostname();
+    }
+
+    /**
+     * @return string
+     */
+    public function getHostname()
+    {
+        if (empty($this->host)) {
+            return $this->defaultHost;
+        }
+
+        return (string) $this->normalizeHostname($this->host);
+    }
+
+    /**
+     * @param string $host
+     *
+     * @return $this
+     *
+     * @throws WrongDataTypeException
+     */
+    public function setHostname($host)
+    {
+        if (!$this->validateHostname($host)) {
+            throw new WrongDataTypeException('Invalid hostname format provided.');
+        }
+
+        $this->host = $this->normalizeHostname($host);
+
+        return $this;
+    }
+
+    /**
+     * @param string $host
+     *
+     * @return bool
+     */
+    private function validateHostname($host)
+    {
+        $host = $this->normalizeHostname($host);
+
+        if (empty($host)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param string $host
+     *
+     * @return string
+     */
+    private function normalizeHostname($host)
+    {
+        $host = trim($host);
+        $host = strtolower($host);
+
+        return $host;
+    }
+
+    /**
+     * @param string $protocol
+     *
+     * @return bool
+     */
+    private function validateProtocol($protocol)
+    {
+        $protocol = strtolower($protocol);
+
+        if (empty($protocol)) {
+            return false;
+        }
+
+        if (!in_array($protocol, $this->validProtocols)) {
+            return false;
+        }
+
+        return true;
     }
 }
